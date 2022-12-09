90c8ef2a9f36f0fc399e4004f1d223eb10c1c3c0
Yun-Hsiang Chan <rayhs.chan@mail.utoronto.ca>
2022-12-08 21:40:46 -0500

    update check

--- PB/startup.sh
+++
@@ -1,8 +0,0 @@
-python3 -m pip install virtualenv
-python3 -m virtualenv -p python3.9 venv
-source venv/bin/activate
-pip install -r requirements.txt
-python PB/manage.py makemigrations
-python PB/manage.py migrate
-# Please create a super user before the server runs 
-python PB/manage.py createsuperuser
