7f357e05f33c64072afe2ae15c09d437ad16a1bb
Yun-Hsiang Chan <rayhs.chan@mail.utoronto.ca>
2022-12-31 19:44:41 -0500

    update initial docker compose for deployment

--- backend/PB/PB/settings.py
+++ backend/PB/PB/settings.py
@@ -172,7 +172,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
+
+STATIC_ROOT = BASE_DIR / "django_static"
 
 MEDIA_URL  = "/media/"
 
